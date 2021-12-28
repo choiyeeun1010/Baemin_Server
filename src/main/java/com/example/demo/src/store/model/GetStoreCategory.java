b891327de8a60d1a2dc1c5d49bef0b0a0bd3415c
choiyeeun1010 <yeeun201010@naver.com>
2021-12-28 12:29:43 +0900

    create storeCategory model

---
+++ src/main/java/com/example/demo/src/store/model/GetStoreCategory.java
@@ -0,0 +1,20 @@
+package com.example.demo.src.store.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GetStoreCategory {
+    private int storeIdx;
+    private String image;
+    private String storeName;
+    private String deliveryTime;
+    private String minAmount;
+    private String deliveryPrice;
+    private float starGrade;
+    private int reviewCount;
+    private String mainMenu;
+}
