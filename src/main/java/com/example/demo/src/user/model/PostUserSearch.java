f48167e32f9d07fa75ba8fdf81f8406b59feb3db
choiyeeun1010 <yeeun201010@naver.com>
2022-01-04 22:22:18 +0900

    create postUserSearch

--- src/main/java/com/example/demo/src/user/model/PostUserSearch.java
+++ src/main/java/com/example/demo/src/user/model/PostUserSearch.java
@@ -1,4 +1,13 @@
 package com.example.demo.src.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class PostUserSearch {
+    private int userIdx;
+    private String searchContents;
 }
